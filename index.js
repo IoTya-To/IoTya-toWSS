2eb168b9d984dc28f7591ecc450c7ced067cf7c5
shiguma127 <shiguma127@gmail.com>
2020-10-29 16:08:00 +0900

    analyze code

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ io.sockets.on('connection', (socket) => {
         console.log(message)
         socket.broadcast.emit('ServerMessage', message)
     })
-    socket.on('roomRequest', (roomname) => {
+    socket.on('createRoom', (roomname) => {
+        console.log(socket.id+" Created room #" +roomname)
         socket.join(roomname)
         socket.emit('ServerMessage', 'you joined ' + roomname)
     })
     socket.on('pushData',(data)=>{
         socket.to(data.room).broadcast.emit(data.data)
     })
-
     socket.on('disconnect', (reason) => {
         console.log(socket.id + ' disconnect : ' + reason)
     })
