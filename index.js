416038ace3dc3cab00f670d8a252039303dedb67
shiguma127 <shiguma127@gmail.com>
2020-11-30 14:58:46 +0900

    add display client num

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const io = require('socket.io').listen(8080)
+let sockets = []
 io.sockets.on('connection', (socket) => {
-    console.log(socket.id + 'is connected')
+    sockets.push(socket.id)
+    sockets.forEach(arr => console.log(arr))
+    console.log(socket.id + 'is connected')    
+    console.log('Client:'+sockets.length)
     socket.on('message', (message) => {
         console.log(message)
         socket.broadcast.emit('ServerMessage', message)
@@ -15,5 +19,7 @@ io.sockets.on('connection', (socket) => {
     })
     socket.on('disconnect', (reason) => {
         console.log(socket.id + ' disconnect : ' + reason)
+        sockets = sockets.filter(sc => sc!==socket.id)
+        console.log('Client:'+sockets.length)
     })
 })
